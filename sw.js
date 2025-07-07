3462d7e13af48c44308cfbb675f11938c5b55dd2
rushikesh-k22 <rushikeshkadam22072006@gmail.com>
2025-07-07 12:19:09 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'tsk-cache-v1';
+const urlsToCache = [
+  './tsk.html',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
